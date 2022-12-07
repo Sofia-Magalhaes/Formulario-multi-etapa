99e6a4d84e953b941563d32679daa94b81ad15dd
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-07 19:56:40 -0300

    setando os svg no sidebar

--- multiform/src/components/SidebarItem/index.tsx
+++ multiform/src/components/SidebarItem/index.tsx
@@ -1,5 +1,8 @@
 import { Link } from 'react-router-dom';
 import * as C from './styles';
+import { ReactComponent as MailIcon } from '../../svgs/mail.svg'
+import { ReactComponent as BookIcon } from '../../svgs/book.svg'
+import { ReactComponent as ProfileIcon } from '../../svgs/profile.svg'
 
 type Props = {
     title: string;
@@ -17,7 +20,15 @@ export const SidebarItem = ({ title, description, icon, path }: Props) => {
                     <C.Description>{description}</C.Description>
                 </C.Info>
                 <C.IconArea>
-
+                    {icon === 'profile' &&
+                        <ProfileIcon fill="white" width={24} height={24} />
+                    }
+                    {icon === 'book' &&
+                        <BookIcon fill="white" width={24} height={24} />
+                    }
+                    {icon === 'mail' &&
+                        <MailIcon fill="white" width={24} height={24} />
+                    }
                 </C.IconArea>
                 <C.Point> </C.Point>
             </Link>
--- multiform/src/components/Theme/index.tsx
+++ multiform/src/components/Theme/index.tsx
@@ -2,12 +2,15 @@ import { ReactNode } from 'react';
 import * as C from './styles';
 import { Header } from '../Header';
 import { SidebarItem } from '../SidebarItem'
+import { useForm } from '../../contexts/FormContext'
 
 type Props = {
     children: ReactNode;
 }
 
 export const Theme = ({ children }: Props) => {
+    const { state } = useForm();
+    
     return (
         <C.Container>
             <C.Area>
