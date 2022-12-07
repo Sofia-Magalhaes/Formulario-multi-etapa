41c3bf62772a067f90c8a6458f51d265deb18ec6
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-06 23:13:21 -0300

    step1

---
+++ multiform/src/components/Theme/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as C from './styles';
+
+export const Header = () => {
+    return (
+        <C.Container>
+            <h1>Cadastro de Desenvolvedor</h1>
+            <p>Faça seu cadastro na lista e receba um empreso agora, confia</p>
+        </C.Container>
+
+    );
+}
--- multiform/src/pages/FormStep1/styles.ts
+++ multiform/src/pages/FormStep1/styles.ts
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    p{
+        font-size:13px;
+        color:#b8b8d4;
+    }
+`;
