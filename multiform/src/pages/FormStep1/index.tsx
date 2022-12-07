41a4c9c7c3f9bbdcdf8d21368eb6935de86e4d1d
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-07 18:33:12 -0300

    criando o passo 2

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -12,7 +12,7 @@ export const FormStep1 = () => {
         dispatch({
             type: FormActions.setCurrentStep,
             payload: 1
-        })
+        });
     }, []);
 
     const handleNextStep = () => {
--- multiform/src/pages/FormStep2/index.tsx
+++ multiform/src/pages/FormStep2/index.tsx
@@ -1,9 +1,44 @@
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
 
-export const FormStep2 = () =>{
+export const FormStep2 = () => {
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 2
+        });
+    }, []);
+
+    const handleNextStep = () => {
+        if (state.name !== '') {
+            navigate('/step3');
+        }else {
+            alert("Preencha os dados");
+        }
+
+    }
+
+ 
+
     return (
-        <C.Container>
-            Passo 2
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 2/3 </p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com o seu nome completo.</p>
+
+                <hr />
+
+              
+
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
     );
 }
