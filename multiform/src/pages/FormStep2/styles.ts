351a0b49e6db8bf61e4f1a11c4eb0c08605737ae
Sofia-Magalhaes <52801509+Sofia-Magalhaes@users.noreply.github.com>
2022-12-07 19:14:11 -0300

    Step 2 finalizado

--- multiform/src/pages/FormStep2/styles.ts
+++ multiform/src/pages/FormStep2/styles.ts
@@ -45,5 +45,11 @@ export const Container = styled.div`
         margin-top:30px;
 
     }
+    .backButton{
+        font-size:16px;
+        text-decoration:none;
+        padding: 20px 40px;
+        color:#b8b8d4;
+    }    
 
 `;
